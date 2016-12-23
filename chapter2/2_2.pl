2f0d930c1d28c1dad44661a37d8ab47936a9085a
gangstar <paizzj@126.com>
2016-12-23 10:54:20 +0800

    read data forom file

--- chapter2/2_2.pl
+++ chapter2/2_2.pl
@@ -1,6 +1,9 @@
 use v6;
 
-my @age = 13, 15, 16, 16, 19 , 20, 20, 21, 22, 22, 25, 25, 25, 30, 33, 33, 35, 35, 35, 35, 36, 40, 45, 46, 52, 70;
+my @age;
+for "age.txt".IO.lines  -> $line {
+    @age.append:  split(',', $line);
+}
 
 my $count =  @age.elems;
 my $sum   = [+] @age;
